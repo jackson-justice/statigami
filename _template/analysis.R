930dce450e175f5e25895521659d20f50eec2970
Jackson Justice <jacksonjustice04@gmail.com>
2026-05-05 09:24:56 -0600

    Add template for analysis in R

---
+++ _template/analysis.R
@@ -0,0 +1,20 @@
+# Project: <project-name>
+# Date: <date>
+
+library(tidyverse)
+
+# --- Load Data ---
+# data <- read_csv("data.csv")
+
+# --- Clean ---
+# clean_data <- data |>
+
+# --- Analysis ---
+# summary <- clean_data |>
+
+# --- Visualization ---
+# ggplot(...) +
+#   ...
+
+# --- Export ---
+# ggsave("figure.png")
